f1bf909db6931d29eaf2875d1f336e505465ac19
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2025-02-22 04:37:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
   credentials: true
 }));
 
